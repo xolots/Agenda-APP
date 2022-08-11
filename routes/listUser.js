475af85aac03c2efb5546a4db10f043699241d9b
dakochan <dakochan64@gmail.com>
2022-08-11 14:07:39 +0700

    route to list all user email and username

---
+++ routes/listUser.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Users = require('../models/user')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const router = express.Router()
+const session = require('express-session')
+const { IsLoggedIn } = require('../middleware')
+const user = require('../models/user')
+
+
+
+router.get('/',IsLoggedIn, async (req, res) => {
+    const users = await Users.find({})
+    const IsAdmin = req.user
+    res.render('listUser', {users, IsAdmin})
+})
+
+
+module.exports = router
