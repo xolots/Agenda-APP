896e994762806c21a200f24844a2fb2d783f6f21
dakochan <dakochan64@gmail.com>
2022-08-12 09:46:43 +0700

    bug fixing - if user was added it will log out current user r and login the new added user

--- routes/register.js
+++ routes/register.js
@@ -2,24 +2,22 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
-const {IsLoggedIn} = require('../middleware')
+const { IsLoggedIn } = require('../middleware')
 
-router.get('/',IsLoggedIn, (req, res) => {
+router.get('/', IsLoggedIn, (req, res) => {
     res.render('users/register')
 })
 
-router.post('/',IsLoggedIn,async (req, res, next) => {
-    try{
-    const {email, username, password} = req.body
-    const user = await new User({email, username})
-    const newUser = await User.register(user, password)
-    req.login(newUser, (err) => {
-        if(err) return next(err);
-        req.flash('success', `Selamat Datang ${newUser.username}`)
-        res.redirect('/dashboard')
-    })
+router.post('/', IsLoggedIn, async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body
+        const user = await new User({ email, username })
+        const newUser = await User.register(user, password)
+        const redirectUrl = req.originalUrl
+        req.flash('success', 'Berhasil Menambah User Baru')
+        res.redirect('/dashboard/user')
 
-    }catch (e){
+    } catch (e) {
         req.flash('error', e.message)
         res.redirect('/register')
     }
